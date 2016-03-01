13a6468258347a97fb327aa7a8f0b3d934bd5b62
maryryan <marym.ryan@hotmail.com>
2016-02-29 20:43:19 -0600

    Houston, we have a table

--- js/script.js
+++ js/script.js
@@ -9,6 +9,7 @@ $(document).ready(function() {
 	// 1.
 	// When the page is loaded,
 	// call the loadData() function.
+	loadData();
 
 });
 
@@ -17,6 +18,11 @@ function loadData() {
 	// 2.
 	// Write an AJAX call here to load your data.
 	// Then PASS the data to writeTable();
+	$.getJSON("js/donations_over_5k.json", function(donationData) {
+
+		writeTable(donationData);
+
+	});
 }
 
 
@@ -29,5 +35,23 @@ function writeTable(data) {
 	// - Contribution Date
 	// - Contributon Information
 	// - Amount
+
+	for (i = 0; i < data.length; i++) {
+		var committee = data[i]["Committee"];
+		var date = data[i]["Contribution Date"];
+		var amount = data[i]["Amount"];
+		var info = data[i]["Contribution Information"];
+
+		if (committee === "CITIZENS TO ELECT KURT SCHAEFER ATTORNEY GENERAL") {
+			$("tbody").append(
+				"<tr>"+
+					"<td>"+date+"</td>"+
+					"<td>"+info+"</td>"+
+					"<td class='moola'>"+amount+"</td>"+
+				"</tr>"
+
+				);
+		}
+	}
 }
 
